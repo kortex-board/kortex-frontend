130a39628b9d4f8dff47741463f44535eb238adc
Pedro Henrique Moreira <pedrohmoreira87@gmail.com>
2026-01-27 11:34:31 -0300

    fix(test): test

--- src/services/api.ts
+++ src/services/api.ts
@@ -7,7 +7,6 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
     const authStore = useAuthStore()
-    // TEST
     const token = authStore.token
     if (token) {
         config.headers.Authorization = `Bearer ${token}`
